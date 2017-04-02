7c5cb98b118acbe1b38ea5eb1c23dd075998d1a7
SKochnev <slava01micki>
2017-04-03 00:37:49 +1000

    Вкладка группы, перенёс кнопки в вкладки, ViewModel для группы

--- students/students/Model/Group.cs
+++ students/students/Model/Group.cs
@@ -1,14 +1,77 @@
-﻿namespace students.Model
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace students.Model
 {
     public class Group : Base
     {
         public Group()
         {
         }
-        public Group(string name)
+        public Group(string fullName)
         {
-            Name = name;
+            FullName = fullName;
         }
-        public string Name { get; set; }
+
+        [Display(Name = "Краткое название")]
+        public string FullName { get
+            {   
+                string _FullName;
+                _FullName = GroupType.Substring(0, 1);
+                string[] _GroupName = GroupName.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                for (int i=0; i< _GroupName.Length; i++)
+                {
+                    _FullName = _FullName + _GroupName[i].ToString().Substring(0, 1);
+                }
+                _FullName = _FullName + Year.Substring(2, 2) + '-' + Number;
+
+                return _FullName ;
+            }
+            set { } }
+
+        [Display(Name = "Тип")]
+        public string GroupType {
+            get
+            {   string[] _GroupType = Name.Split(new char[] { '_' }, StringSplitOptions.RemoveEmptyEntries);
+                return _GroupType[0].ToString();
+            }
+            set { }
+        }
+
+        [Display(Name = "Название")]
+        public string GroupName {
+            get
+            {
+                string[] _GroupName = Name.Split(new char[] { '_' }, StringSplitOptions.RemoveEmptyEntries);
+                return _GroupName[1].ToString();
+            }
+            set { }
+        }
+
+        [Display(Name = "Год")]
+        public string Year {
+            get
+            {
+                string[] _Year = Name.Split(new char[] { '_' }, StringSplitOptions.RemoveEmptyEntries);
+                return _Year[2].ToString();
+            }
+            set { }
+        }
+
+        [Display(Name = "Подгруппа")]
+        public string Number {
+            get
+            {
+                string[] _Number = Name.Split(new char[] { '_' }, StringSplitOptions.RemoveEmptyEntries);
+                return _Number[3].ToString();
+            }
+            set { }
+        }
+
+        public string Name { get
+            {
+                return GroupType + '_' + GroupName + '_' + Year + '_' + Number;
+            }
+            set{}}
     }
 }
---
+++ students/students/ViewModel/GroupViewModel.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Data.Entity.Migrations;
+
+using DevExpress.Mvvm.DataAnnotations;
+using DevExpress.Mvvm.POCO;
+using DevExpress.Mvvm;
+using DevExpress.Mvvm.Native;
+using DevExpress.Xpf.CodeView;
+
+using students.Model;
+
+namespace group.ViewModel
+{
+    [POCOViewModel]
+    public partial class GroupViewModel
+    {
+
+        public GroupViewModel()
+        {
+            Model = new MainModel();
+            GroupList = new ObservableCollection<Group>(Model.GroupRepository);
+        }
+
+        private MainModel Model { get; set; }
+
+        public static GroupViewModel Create() => ViewModelSource.Create(() => new GroupViewModel());
+
+              
+        [BindableProperty]
+        public virtual ObservableCollection<Group> GroupList { get; set; }
+        [BindableProperty]        
+        public virtual Group SelectedGroup{ get; set; }
+
+        [Command]
+        public void Save()
+        {
+            Model.GroupRepository.AddOrUpdate(GroupList.ToArray());
+            Model.SaveChanges();
+        }
+
+        [Command]
+        public void Refresh()
+        {
+            GroupList.Clear();
+            GroupList.AddRange(Model.GroupRepository.ToList());
+        }
+        [Command]
+        public void NewGroup()
+        {
+            var newGroup = Model.GroupRepository.Create();
+            Model.GroupRepository.Add(newGroup);
+            Model.SaveChanges();
+            GroupList.Add(newGroup);
+            Refresh();
+        }
+        [Command]
+        public void DeleteGroup(Group selectedGroup)
+        {
+            if (selectedGroup == null) return;
+            Model.GroupRepository.Remove(selectedGroup);
+            Model.SaveChanges();
+            Refresh();
+        }
+                          
+      
+
+    }
+}
