030c280e409fa3501e32bb6a0f0c7bad452262f4
Alheinochka1 <Alheinochka1@KURSIZ-02>
2017-04-08 14:46:25 +1000

    Вкладка "Предметы" / Класс предмет (+ коллекция групп и преподов)

---
+++ students/students/Model/Subject.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using DevExpress.Mvvm.DataAnnotations;
+using DevExpress.Mvvm;
+using System.Collections.ObjectModel;
+
+namespace students.Model
+{ 
+    public class Subject : Base
+    {
+        [Display(Name = "Название")]
+        public string Name { get; set; }
+
+        [Display(Name = "Тип")]
+        public string Type { get; set; }
+
+        [Display(Name = "Аудитория")]
+        public string LectureHall { get; set; }
+
+        public ObservableCollection<Group> SubjectOfGroup { get; set; }
+        public ObservableCollection<Teacher> SubjectOfTeacher { get; set; }
+    }
+}
